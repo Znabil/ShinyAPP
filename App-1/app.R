dbf656448146cc3ffe226021effe2395abccde55
Znabil <aziznabil126@gmail.com>
2019-12-01 23:54:28 +0100

    uploading Shiny files to gethub repo

---
+++ App-1/app.R
@@ -0,0 +1,49 @@
+
+library(shiny)
+
+# Define UI for the app 
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Hello World!"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 5,
+                  max = 50,
+                  value = 30)
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Histogram ----
+      plotOutput(outputId = "distPlot")
+      
+    )
+  )
+)
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+
+  output$distPlot <- renderPlot({
+    
+    x    <- faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    hist(x, breaks = bins, col = "#75AADB", border = "orange",
+         xlab = "Waiting time to next eruption (in mins)",
+         main = "Histogram of waiting times")
+    
+  })
+  
+}
+shinyApp(ui = ui, server = server)
