667b770e919394b74ebcec242488f34ea4dc0958
Znabil <aziznabil126@gmail.com>
2019-12-02 00:41:23 +0100

    updating files regarding deployment of APPS

--- App-7/App.R
+++ App-7/App.R
@@ -1,1 +1,16 @@
- 
+library(rsconnect)
+library(curl)
+library(httr)
+library(shiny)
+#running Application published  github repo 
+runGitHub( "ShinyAPP", "Znabil",subdir = "App-1")
+runGitHub( "ShinyAPP", "Znabil",subdir = "App-2")
+runGitHub( "ShinyAPP", "Znabil",subdir = "App-3")
+runGitHub( "ShinyAPP", "Znabil",subdir = "App-4")
+runGitHub( "ShinyAPP", "Znabil",subdir = "App-5")
+runGitHub( "ShinyAPP", "Znabil",subdir = "App-6")
+#publishing APP on shinyapps.io using the rsconnect library
+rsconnect::deployApp('/media/nabil/HDD1/BigData/Visualization/ShinyAPP/App-5/')
+#running the APP From the shinyApps.io Url as HTLM Page
+browseURL( "https://aziznabil.shinyapps.io/app-5/")
+
